0bcd9fd6f8ab8eadc0ff0be064ca8dc81d36112e
rom19199 <54779748+rom19199@users.noreply.github.com>
2021-03-17 21:45:11 -0600

    Add files via upload

---
+++ src/components/login/register.jsx
@@ -0,0 +1,93 @@
+import React from "react";
+import loginImg from "../../login.svg";
+
+export class Register extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      UserName: "",
+      //lastN: "",
+      password: "",
+      //gender: "",
+
+
+  }
+  this.handleSubmit=this.handleSubmit.bind(this)
+}
+
+firsthandler = (event) => {
+  this.setState({
+      UserName: event.target.value
+  })
+}
+passwordhandler = (event) => {
+  this.setState({
+      password: event.target.value
+  })
+}
+// passwordhandler = (event) => {
+//   this.setState({
+//       password: event.target.value
+//   })
+// }
+
+// genderhandler = (event) => {
+//   this.setState({
+//       gender: event.target.value
+//   })
+// }
+
+handleSubmit = (event) => {
+  alert(`${this.state.UserName} ${this.state.password}  Registered Successfully !!!!`)
+  console.log(this.state);
+  this.setState({
+      UserName: "",
+      //lastName: "",
+      password: '',
+      //gender: "",
+  })
+event.preventDefault()
+  
+}
+
+  
+
+  render() {
+    return (
+      <div className="base-container" ref={this.props.containerRef}>
+        <form onSubmit={this.handleSubmit}>
+        <div className="header">Register</div>
+        <div className="content">
+          <div className="image">
+            <img src={loginImg} />
+          </div>
+          <div className="form">
+           
+              <div className="form-group">
+                <label htmlFor="username">Username</label>
+                <input type="text" value={this.state.UserName} onChange={this.firsthandler} name="username" placeholder="username" />
+              </div>
+              {/* <div className="form-group">
+              <label htmlFor="email">Email</label>
+              <input type="text" name="email" placeholder="email" />
+            </div> */}
+              <div className="form-group">
+                <label htmlFor="password">Password</label>
+                <input type="password" value={this.state.password} onChange ={this.passwordhandler} name="password" placeholder="password" />
+              </div>
+            
+            
+          </div>
+          
+        </div>
+        <div className="footer">
+          <input type="submit" value = "submit"/>
+            
+          
+        </div>
+        </form>
+      </div>
+    );
+  }
+}
